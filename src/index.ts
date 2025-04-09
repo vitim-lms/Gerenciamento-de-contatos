2990ec33a7f8e69e4f9ceb058024aac6b4f842f4
Willian Marques <willian.goncalves4@gmail.com>
2025-04-09 20:19:31 -0300

    feat: add support to receive form data

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,12 @@ app.set('view engine', 'ejs');
 
 app.set('views', './public');
 
-app.get('/', function(req, res) {
-    res.render('index', { message: 'hello' });
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', function (req, res) {
+    res.render('index', { message: 'Hello' });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('Servidor rodando no endereço http://localhost:3333');
+});
